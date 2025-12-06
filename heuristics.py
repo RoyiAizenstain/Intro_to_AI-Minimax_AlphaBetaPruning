021f5535099d796b64dd64babc89822b9c43c2e6
Royi <Royi.aizen@gmail.com>
2025-12-06 19:29:32 +0200

    Refactor game engine to enable heuristic-based gameplay and implement Alpha-Beta pruning logic

--- heuristics.py
+++ heuristics.py
@@ -1,8 +1,20 @@
+def count_moves_for_both_players(state):
+    current_player = state.get_curr_player()
+    
+    current_moves = len(state.potential_moves())
+    
+    state.set_curr_player((current_player % 2) + 1)
+    opponent_moves = len(state.potential_moves())
+    
+    state.set_curr_player(current_player)
+    
+    return (current_moves, opponent_moves)
 
 def base_heuristic(curr_state):
-    #add code here
-    return 0
-    pass
+    (current_moves, opponent_moves) = count_moves_for_both_players(curr_state)
+    return current_moves - opponent_moves
+   
+
 
 def advanced_heuristic(curr_state):
     return 0
